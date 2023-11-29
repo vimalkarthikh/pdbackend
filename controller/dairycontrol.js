d9c96f944720bbf0f8a4f0cc86912a8a1f222b1e
Vimal Karthik H <137371585+vimalkarthikh@users.noreply.github.com>
2023-11-29 22:57:20 +0530

    Update dairycontrol.js

--- controller/dairycontrol.js
+++ controller/dairycontrol.js
@@ -1,7 +1,7 @@
 import { Dairy } from "../models/dairy.js";
 
 export function getAllUserDocs(req){
-    return Dairy.find({user:req.user._id}).populate( "user","title doc");               
+    return Dairy.find({user:req.user._id}).populate( "user","title document date");               
 
 }
 
